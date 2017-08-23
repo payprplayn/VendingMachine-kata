34e056d729620ec43bca394df9d7dd14643d9b8d
Gabriel Burns <payprplayn@gmail.com>
2017-08-23 14:51:53 -0500

    coins are returned when coin return button is pressed

--- src/main/VendingMachine.java
+++ src/main/VendingMachine.java
@@ -1,13 +1,17 @@
 package main;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
 public class VendingMachine {
 
 	private Collection<? super Coin> coinReturn;
 	private int balance;
+	private Collection<Coin> depositedCoins;
 	public VendingMachine(){
 		balance=0;
+		depositedCoins=new LinkedList<Coin>();
+		
 	}
 	/**
 	 * @return the current contents of this VendingMachine's display
@@ -24,11 +28,16 @@ public class VendingMachine {
 	public void insert(Coin coin) {
 		if(coin.getWeightInGrams()==5.67 && coin.getDiameterInMillimeters()==24.26 && coin.getThicknessInMillimeters()==1.75){//detect a Quarter
 			balance+=25;
+			depositedCoins.add(coin);
 		}
 		else if(coin.getWeightInGrams()==5.0 && coin.getDiameterInMillimeters()==21.21 && coin.getThicknessInMillimeters()==1.95){//detect a nickel
 			balance+=5;
+			depositedCoins.add(coin);
 		}
-		else if(coin.getWeightInGrams()==2.268 && coin.getDiameterInMillimeters()==17.91 && coin.getThicknessInMillimeters()==1.35);
+		else if(coin.getWeightInGrams()==2.268 && coin.getDiameterInMillimeters()==17.91 && coin.getThicknessInMillimeters()==1.35){//detect a dime
+			balance+=10;
+			depositedCoins.add(coin);
+		}
 		else coinReturn.add(coin);
 		
 	}
@@ -36,7 +45,8 @@ public class VendingMachine {
 	/**
 	 * Sets the coin return destination for this VendingMachine.
 	 * @param target 
-	 * the new coin return destination.  Any future coins returned by this Vending Machine will be added to this Collection. 
+	 * the new coin return destination.  Any future coins returned by this Vending Machine will be added to this Collection.
+	 * Must support add().
 	 */
 	public void setCoinReturn(Collection<? super Coin> target) {
 		coinReturn=target;	
@@ -46,6 +56,15 @@ public class VendingMachine {
 	 */
 	public Collection<? super Coin> getCoinReturn() {
 		return coinReturn;
+	}
+	
+	/**
+	 * press the coin return button on this Vending Machine
+	 */
+	public void returnCoins() {
+		balance=0;
+		coinReturn.addAll(depositedCoins);
+		depositedCoins.clear();
 	}
 
 }
--- src/test/VendingMachineTest.java
+++ src/test/VendingMachineTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
 import java.util.LinkedList;
 
 import org.junit.Before;
@@ -12,13 +13,13 @@ import main.VendingMachine;
 
 public class VendingMachineTest {
 	VendingMachine vendingMachine;
-	private enum CoinTypes implements Coin{
+	private enum CoinType implements Coin{
 		PENNY(2.5,1.55,19.05), QUARTER(5.67,1.75,24.26), NICKEL(5.0,1.95,21.21), DIME(2.268, 1.35, 17.91);
 		private double diameter;
 		private double thickness;
 		private double weight;
 
-		CoinTypes(double weight,double thickness, double diameter){
+		CoinType(double weight,double thickness, double diameter){
 			this.weight=weight;
 			this.thickness=thickness;
 			this.diameter=diameter;
@@ -51,39 +52,57 @@ public class VendingMachineTest {
 	
 	@Test
 	public void vendingMachineRejectsPennies(){
-		vendingMachine.insert(CoinTypes.PENNY);
-		assert(vendingMachine.getCoinReturn().contains(CoinTypes.PENNY));
+		vendingMachine.insert(CoinType.PENNY);
+		assert(vendingMachine.getCoinReturn().contains(CoinType.PENNY));
 	}
 	
 	@Test
 	public void vendingMachineAcceptsQuarters(){
-		vendingMachine.insert(CoinTypes.QUARTER);
-		assert(!vendingMachine.getCoinReturn().contains(CoinTypes.QUARTER));
+		vendingMachine.insert(CoinType.QUARTER);
+		assert(!vendingMachine.getCoinReturn().contains(CoinType.QUARTER));
 	}
 	
 	@Test
 	public void vendingMachineAcceptsNickels(){
-		vendingMachine.insert(CoinTypes.NICKEL);
-		assert(!vendingMachine.getCoinReturn().contains(CoinTypes.NICKEL));
+		vendingMachine.insert(CoinType.NICKEL);
+		assert(!vendingMachine.getCoinReturn().contains(CoinType.NICKEL));
 	}
 	@Test
 	public void vendingMachineAcceptsDimes(){
-		vendingMachine.insert(CoinTypes.DIME);
-		assert(!vendingMachine.getCoinReturn().contains(CoinTypes.DIME));
+		vendingMachine.insert(CoinType.DIME);
+		assert(!vendingMachine.getCoinReturn().contains(CoinType.DIME));
 	}
 	@Test
 	public void displayedTotalIncreasesBy25WhenQuarterIsAccepted(){
-		vendingMachine.insert(CoinTypes.QUARTER);
+		vendingMachine.insert(CoinType.QUARTER);
 		assertEquals("$0.25",vendingMachine.readDisplay());
-		vendingMachine.insert(CoinTypes.QUARTER);
+		vendingMachine.insert(CoinType.QUARTER);
 		assertEquals("$0.50",vendingMachine.readDisplay());
 	}
 	@Test
 	public void displayedTotalIncreasesBy5WhenNickelIsAccepted(){
-		vendingMachine.insert(CoinTypes.NICKEL);
+		vendingMachine.insert(CoinType.NICKEL);
 		assertEquals("$0.05",vendingMachine.readDisplay());
-		vendingMachine.insert(CoinTypes.NICKEL);
+		vendingMachine.insert(CoinType.NICKEL);
 		assertEquals("$0.10",vendingMachine.readDisplay());
 	}
+	@Test
+	public void displayedTotalIncreasesBy5WhenDimeIsAccepted(){
+		vendingMachine.insert(CoinType.DIME);
+		assertEquals("$0.10",vendingMachine.readDisplay());
+		vendingMachine.insert(CoinType.DIME);
+		assertEquals("$0.20",vendingMachine.readDisplay());
+	}
+	@Test
+	public void coinsAreReturnedWhenCoinReturnButtonIsPushed(){
+		vendingMachine.insert(CoinType.DIME);
+		vendingMachine.insert(CoinType.QUARTER);
+		vendingMachine.returnCoins();
+		assert vendingMachine.getCoinReturn().remove(CoinType.DIME);
+		assert vendingMachine.getCoinReturn().remove(CoinType.QUARTER);
+		assert vendingMachine.getCoinReturn().isEmpty();
+	}
+
+	
 
 }
